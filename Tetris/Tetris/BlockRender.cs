07ee2124538a739f69d9b747c953ea8a0308d714
Alperen <alperenelver@gmail.com>
2017-10-04 16:35:29 +0200

    Render class toegevoegd

---
+++ Tetris/Tetris/BlockRender.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tetris
+{
+
+    public class BlockRender
+    {
+        GraphicsDeviceManager graphics;
+        //Camera
+        Vector3 camTarget;
+        Vector3 camPosition;
+        Matrix projectionMatrix;
+        Matrix viewMatrix;
+        Matrix worldMatrix;
+        Model model;
+        bool orbit = false;
+
+
+
+        protected override void Initialize()
+        {
+
+
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+
+            //Setup Camera
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, -3f, -10f);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), graphics.GraphicsDevice.Viewport.AspectRatio, 1f, 1000f);
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, new Vector3(0f, 1f, 0f));// Y up
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.Forward, Vector3.Up);
+            model = Content.Load<Model>("MonoCube");
+
+        }
+
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                camPosition.X -= 0.1f;
+                camTarget.X -= 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                camPosition.X += 0.1f;
+                camTarget.X += 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                camPosition.Y -= 0.1f;
+                camTarget.Y -= 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                camPosition.Y += 0.1f;
+                camTarget.Y += 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+            {
+                camPosition.Z += 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+            {
+                camPosition.Z -= 0.1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                orbit = !orbit;
+                if (Keyboard.GetState().IsKeyUp(Keys.Space))
+                    orbit = !orbit;
+            }
+
+            if (orbit)
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(MathHelper.ToRadians(1f));
+
+                camPosition = Vector3.Transform(camPosition, rotationMatrix);
+            }
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, Vector3.Up);
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.AmbientLightColor = new Vector3(-10f, 20, 0);
+                    effect.View = viewMatrix;
+                    effect.World = worldMatrix;
+                    effect.Projection = projectionMatrix;
+                }
+                mesh.Draw();
+
+
+                base.Draw(gameTime);
+            }
+        }
+    }
+}
